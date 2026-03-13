6872dd5354ac10795c38805338d79946dcdb1fd8
Akshay Bairu Patgar <akshaybairupatgar@Akshay-Bairu-Patgar-B1060s-MacBook-Pro.local>
2026-03-13 16:45:02 +0530

    configuring deployment to render

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor(),new AllResponseTransformInterceptor(reflector));
   app.useGlobalFilters(new AllExceptionsFilter());
   const configService = app.get(ConfigService);
-  await app.listen(configService.get<number>('env.port') || 3000 , '0.0.0.0');
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT , '0.0.0.0');
 }
 bootstrap();
